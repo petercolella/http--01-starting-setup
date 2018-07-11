19e2ea8f6bb427eb50693292a73eeb965186ff22
petercolella <peter.joseph.colella@gmail.com>
2018-07-10 21:18:48 -0400

    Lecture 152

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,14 +14,21 @@ class Blog extends Component {
     componentDidMount () {
         axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data})
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Peter'
+                    }
+                })
+                this.setState({posts: updatedPosts})
                 // console.log(response);
             });
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} />;
+            return <Post key={post.id} title={post.title} author={post.author} />;
         });
 
         return (
