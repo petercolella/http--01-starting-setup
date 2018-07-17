0662acc66011d628df1636fc1532d6adae1dc802
petercolella <peter.joseph.colella@gmail.com>
2018-07-16 23:24:47 -0400

    Lecture 184

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -10,16 +10,20 @@ class NewPost extends Component {
         author: 'Max'
     }
 
+    componentDidMount () {
+        console.log( this.props );
+    }
+
     postDataHandler = () => {
         const data = {
             title: this.state.title,
             body: this.state.content,
             author: this.state.author
         };
-        axios.post('/posts', data)
-            .then(response => {
-                console.log(response);
-            });
+        axios.post( '/posts', data )
+            .then( response => {
+                console.log( response );
+            } );
     }
 
     render () {
@@ -27,11 +31,11 @@ class NewPost extends Component {
             <div className="NewPost">
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title} onChange={( event ) => this.setState( { title: event.target.value } )} />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content} onChange={( event ) => this.setState( { content: event.target.value } )} />
                 <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                <select value={this.state.author} onChange={( event ) => this.setState( { author: event.target.value } )}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
