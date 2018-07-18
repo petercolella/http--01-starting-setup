72fb0aff9e8d3b825a63bd242ca736d5ecbe8c1a
petercolella <peter.joseph.colella@gmail.com>
2018-07-17 23:01:00 -0400

    Lecture 190

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -7,32 +7,33 @@ class FullPost extends Component {
     state = {
         loadedPost: null
     }
-    
-    componentDidUpdate () {
-        if (this.props.id) {
-            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
-                axios.get('/posts/' + this.props.id)
-                .then(response => {
-                    // console.log(response);
-                    this.setState({loadedPost: response.data});
-                });
+
+    componentDidMount () {
+        console.log( this.props );
+        if ( this.props.match.params.id ) {
+            if ( !this.state.loadedPost || ( this.state.loadedPost && this.state.loadedPost.id !== this.props.id ) ) {
+                axios.get( '/posts/' + this.props.match.params.id )
+                    .then( response => {
+                        // console.log(response);
+                        this.setState( { loadedPost: response.data } );
+                    } );
             }
         }
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
-            .then(response => {
-                console.log(response);
-            });
+        axios.delete( '/posts/' + this.props.id )
+            .then( response => {
+                console.log( response );
+            } );
     }
 
     render () {
-        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if (this.props.id) {
-            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+        if ( this.props.id ) {
+            post = <p style={{ textAlign: 'center' }}>Loading...</p>;
         }
-        if (this.state.loadedPost) {
+        if ( this.state.loadedPost ) {
             post = (
                 <div className="FullPost">
                     <h1>{this.state.loadedPost.title}</h1>
@@ -41,7 +42,7 @@ class FullPost extends Component {
                         <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
-    
+
             );
         }
         return post;
